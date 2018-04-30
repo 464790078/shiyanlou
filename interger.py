3c13283f2f4b964fd05a4bc5435ea7894d8febca
464790078 <yunf_ei@163.com>
2018-04-30 11:50:15 +0800

    New edition

--- interger.py
+++ interger.py
@@ -3,3 +3,8 @@ days = int(input("Enter days: "))
 months = days // 30
 days = days % 30
 print("Months = {} Days= {}".format(months,days))
+numbers = int(input("please input your number:"))
+if numbers <= 100:
+    print("your number is smaller than 100")
+else:
+    print("your number is bigger than 100")
